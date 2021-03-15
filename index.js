18535893d07244604e3374d19fc94adc999e9a71
Erik <erikenz2001@gmail.com>
2021-03-14 21:43:34 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.once('ready', ()=>{
+    console.log('G4G Bot is online')
+})
+
+
+client.login('Nzk0MDAyNjYyMTk3ODIxNDgy.X-0emg.21hIUv6Nzsw9Tw11saSfHOONqU4');
